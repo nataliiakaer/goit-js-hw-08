aca26eb73b079cf2d1761229f709859dbc7e0ca2
Nataliia Kruhliak <nataliia.kaer@gmail.com>
2023-04-08 18:40:53 +0300

    first task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,43 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+// Описаний в документації
+import SimpleLightbox from 'simplelightbox';
+// Додатковий імпорт стилів
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 console.log(galleryItems);
+
+const galleryListEl = document.querySelector('.gallery');
+const imageMarkup = createImageItemMurkup(galleryItems);
+
+galleryListEl.insertAdjacentHTML('beforeend', imageMarkup);
+galleryListEl.addEventListener('click', onImageClick);
+
+function createImageItemMurkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      // console.log(preview);
+      return `
+        <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+      </li>`;
+    })
+    .join('');
+}
+
+function onImageClick(evt) {
+  evt.preventDefault();
+  const isSelectedImageEl = evt.target.classList.contains('gallery__image');
+
+  if (!isSelectedImageEl) {
+    return;
+  }
+}
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  scrollZoom: false,
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,23 @@
+import throttle from 'lodash.throttle';
 
+const formEl = document.querySelector('.feedback-form');
+let inputValue = {
+  email: '',
+  message: '',
+};
+
+formEl.form.addEventListener('submit', onFormSubmit);
+formEl.form.addEventListener('input', throttle(onFormInput, 1));
+
+function onFormSubmit(evt) {
+  evt.preventDefaukt();
+
+  evt.currentTarget.reset();
+  localStorage.removeItem('feedback-form-state');
+}
+
+function onFormInput(evn) {
+  const message = evn.currentTarget.value;
+  console.log(message);
+  localStorage.setItem('feedback-form-state', message);
+}
