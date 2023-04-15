99fde5d63e654114af430696ee82dc3d7a7a99d0
Nataliia Kruhliak <nataliia.kaer@gmail.com>
2023-04-15 10:24:09 +0300

    Task 2 - video

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import Vimeo, { Player } from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo(iframe);
+const STORAGE_CURRENT_TIME = 'videoplayer-current-time';
+// console.log(iframe);
+
+const onPlay = function (data) {
+  localStorage.setItem(STORAGE_CURRENT_TIME, data.seconds);
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+const currentTime = JSON.parse(localStorage.getItem(STORAGE_CURRENT_TIME));
+console.log('Current seconds time is', currentTime);
+if (currentTime !== null) {
+  player.setCurrentTime(currentTime);
+}
